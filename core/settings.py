2b3e78246a33d366f46c8986b59fd3fe3bac196c
Elvan Hidayat <elvanhd@gmail.com>
2023-04-10 14:18:30 +0700

    listings urls and template

--- core/settings.py
+++ core/settings.py
@@ -35,6 +35,8 @@ ALLOWED_HOSTS = ["*"]
 
 INSTALLED_APPS = [
     "pages.apps.PagesConfig",
+    "listings.apps.ListingsConfig",
+    "realtors.apps.RealtorsConfig",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
@@ -121,8 +123,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATIC_URL = '/assets/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'core/static')
-]
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATIC_URL = "/assets/"
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "core/static")]
--- core/urls.py
+++ core/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('', include('pages.urls')),
-    path('admin/', admin.site.urls),
+    path("", include("pages.urls")),
+    path("listings/", include("listings.urls")),
+    path("admin/", admin.site.urls),
 ]
