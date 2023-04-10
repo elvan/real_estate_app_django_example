71a3c6fc8c904306eae85c51b8827e81909b3860
Elvan Hidayat <elvanhd@gmail.com>
2023-04-11 01:10:30 +0700

    accounts app and urls

---
+++ accounts/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import redirect, render
+
+
+def register(request):
+    return render(request, "accounts/register.html")
+
+
+def login(request):
+    return render(request, "accounts/login.html")
+
+
+def logout(request):
+    return redirect("index")
+
+
+def dashboard(request):
+    return render(request, "accounts/dashboard.html")
--- core/settings.py
+++ core/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     "pages.apps.PagesConfig",
     "listings.apps.ListingsConfig",
     "realtors.apps.RealtorsConfig",
+    "accounts.apps.AccountsConfig",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
