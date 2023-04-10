fe6647802d9e1b0a16e05449f0cc5de70a147374
Elvan Hidayat <elvanhd@gmail.com>
2023-04-10 13:59:59 +0700

    fix assets paths

--- core/settings.py
+++ core/settings.py
@@ -122,7 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATIC_URL = '/static/'
+STATIC_URL = '/assets/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'core/static')
 ]
