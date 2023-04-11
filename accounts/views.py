b1b1f14300498e92143df35d2bde5c20ed5775fe
Elvan Hidayat <elvanhd@gmail.com>
2023-04-11 09:03:08 +0700

    fix redirect when logging out

--- accounts/views.py
+++ accounts/views.py
@@ -68,7 +68,8 @@ def logout(request):
     if request.method == "POST":
         auth.logout(request)
         messages.success(request, "You are now logged out")
-        return redirect("index")
+
+    return redirect("index")
 
 
 def dashboard(request):
